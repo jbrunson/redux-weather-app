8be5795d19fea3923d0d73b63a6d5cc62d1928e0
Jeff Brunson <jeff.a.brunson@gmail.com>
2016-02-15 12:39:02 -0700

    app setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
+
+export default App;
